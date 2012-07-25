18cdc9b529c2c7ab0a61a7d15e5a34b8be726a50
amauch3 <amauch3@gmail.com>
2012-07-25 05:51:19 +0000

    -changed how a car object is constructed, finished the DataControl.cs class-

--- branches/4780_final_car_POS/4780_final_car_POS/car.cs
+++ branches/4780_final_car_POS/4780_final_car_POS/car.cs
@@ -7,25 +7,17 @@ using System.Text;
     {
         #region constructor
        
-            public car(int passedMake, int passedModel, int passedYear, string passedVin, decimal passedPrice)
+            public car(int passedModel, int passedYear, decimal passedPrice)
             {
                 //sets the attributes of the car
-                make = passedMake;
                 model = passedModel;
                 year = passedYear;
-                vin = passedVin;
                 price = passedPrice;
-
             }
 
         #endregion
 
         #region variables/properties
-        
-            /// <summary>
-            /// Stores the make of the car
-            /// </summary>
-            public int make {get; set;}
 
             /// <summary>
             /// Stores the model of the car
@@ -36,11 +28,6 @@ using System.Text;
             /// stores the year of the car
             /// </summary>
             public int year { get; set; }
-
-            /// <summary>
-            /// Stores the vin of the car
-            /// </summary>
-            public string vin { get; set; }
 
             /// <summary>
             /// Stores the price of the car
