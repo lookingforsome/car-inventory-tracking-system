8f398b4ef4b214a7cc7af4db08ce89b55624e228
sam.mtalbot <sam.mtalbot@gmail.com>
2012-08-05 15:49:52 +0000

    frmSearch updates

--- branches/4780_final_car_POS/4780_final_car_POS/frmSearch.cs
+++ branches/4780_final_car_POS/4780_final_car_POS/frmSearch.cs
@@ -28,112 +28,177 @@ namespace _4780_final_car_POS
 
 		#endregion
 
+		#region Constructor(s)
+
 		public frmSearch()
 		{
 			InitializeComponent();
 
-			BindingList<Invoice> Invoices = DataControl.getAllInvoices();
-
-			//string InvoiceKey, string CustomerKey, string SalesPersonKey, string PurchaseDate, string Cost
-
-			//Create 4 columns to be displayed in the DataGridView
-			DataGridViewTextBoxColumn InvoiceKeyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			DataGridViewTextBoxColumn CustomerNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			DataGridViewTextBoxColumn SalesPersonName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			DataGridViewTextBoxColumn PurchaseDateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			DataGridViewTextBoxColumn CostColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			DataGridViewButtonColumn SelectInvoiceButtonColumn = new System.Windows.Forms.DataGridViewButtonColumn();
-
-			//Set the properties of the columns.
-			//Note the property "DataPropertyName".  
-			//The value of this property corresponds to the name of the property in the class Employee.  
-			//This binds this column to that field in the class.
-
-			InvoiceKeyColumn.DataPropertyName = "InvoiceKey";
-			InvoiceKeyColumn.HeaderText = "Invoice ID";
-			InvoiceKeyColumn.Name = "InvoiceID";
-
-			CustomerNameColumn.DataPropertyName = "customerName";
-			CustomerNameColumn.HeaderText = "Customer Full Name";
-			CustomerNameColumn.Name = "customerName";
-			//CustomerFirstNameColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-			//CustomerFirstNameColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-
-			SalesPersonName.DataPropertyName = "salesPersonName";
-			SalesPersonName.HeaderText = "Sales Person Name";
-			SalesPersonName.Name = "salesPersonName";
-
-			PurchaseDateColumn.DataPropertyName = "PurchaseDate";
-			PurchaseDateColumn.HeaderText = "Purchase Date";
-			PurchaseDateColumn.Name = "PurchaseDate";
-
-			CostColumn.DataPropertyName = "Cost";
-			CostColumn.HeaderText = "Total Cost";
-			CostColumn.Name = "Cost";
-
-			SelectInvoiceButtonColumn.HeaderText = "Select Invoice";
-			SelectInvoiceButtonColumn.Name = "SelectInvoice";
-			SelectInvoiceButtonColumn.Text = "Select the Invoice";
-			SelectInvoiceButtonColumn.UseColumnTextForButtonValue = true;
-			SelectInvoiceButtonColumn.Width = 200;
-
-			//Add the columns to the DataGridView
-			InvoiceDataGridView.Columns.Add(InvoiceKeyColumn);
-			InvoiceDataGridView.Columns.Add(CustomerNameColumn);
-			InvoiceDataGridView.Columns.Add(SalesPersonName);
-			InvoiceDataGridView.Columns.Add(PurchaseDateColumn);
-			InvoiceDataGridView.Columns.Add(CostColumn);
-			InvoiceDataGridView.Columns.Add(SelectInvoiceButtonColumn);
-
-			//Don't generate columns automatically
-			InvoiceDataGridView.AutoGenerateColumns = false;
-
-			//Set the DataSource for the DataGridView
-			InvoiceDataGridView.DataSource = Invoices;
-
-
+			//populate/create datagridview data
+			fillDataGridView();
 
 			//instantiate the Invoice Number dropdown
-			da = new clsDataAccess(); //Data Access object to query database
-			DataSet ds; //Data set to store results of SQL statement
-			string sSQL = "SELECT DISTINCT InvoiceKey FROM Invoices"; //SQL statement to execute
-
-			int iTotalResults = 0;  //Number of results that came back from SQL statement
-
-			//Run the statement and store the result into the dataset
-			ds = da.ExecuteSQLStatement(sSQL, ref iTotalResults);
-
-			//For each row (flight) in the data set, add a new item to the combo box
-			foreach (DataRow dr in ds.Tables[0].Rows)
-				cmbInvoiceNumber.Items.Add(dr["InvoiceKey"].ToString());
+			fillInvoiceDropdown();
 
 			//instantiate the cost dropdown
-
-			string sCostSQL = "SELECT DISTINCT TotalCost FROM Invoices"; //SQL statement to execute
-
-			iTotalResults = 0;  //Number of results that came back from SQL statement
-
-			//Run the statement and store the result into the dataset
-			ds = da.ExecuteSQLStatement(sCostSQL, ref iTotalResults);
-
-			//For each row (flight) in the data set, add a new item to the combo box
-			foreach (DataRow dr in ds.Tables[0].Rows)
-				cmbTotalCost.Items.Add(dr["TotalCost"].ToString());
-
+			fillCostDropdown();
 
 			//instantiate the date dropdown.
-
-			string sDateSQL = "SELECT DISTINCT PurchaseDate FROM Invoices"; //SQL statement to execute
-
-			iTotalResults = 0;  //Number of results that came back from SQL statement
-
-			//Run the statement and store the result into the dataset
-			ds = da.ExecuteSQLStatement(sDateSQL, ref iTotalResults);
-
-			//For each row (flight) in the data set, add a new item to the combo box
-			foreach (DataRow dr in ds.Tables[0].Rows)
-				cmbInvoiceDate.Items.Add(dr["PurchaseDate"].ToString());
+			fillDateDropdown();
 		}
+		
+		#endregion
+
+		#region Methods
+
+		public void fillDataGridView()
+		{
+			try
+			{
+				BindingList<Invoice> Invoices = DataControl.getAllInvoices();
+
+				//Create 4 columns to be displayed in the DataGridView
+				DataGridViewTextBoxColumn InvoiceKeyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+				DataGridViewTextBoxColumn CustomerNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+				DataGridViewTextBoxColumn SalesPersonName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+				DataGridViewTextBoxColumn PurchaseDateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+				DataGridViewTextBoxColumn CostColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+				DataGridViewButtonColumn SelectInvoiceButtonColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+
+				//Set the properties of the columns.
+				//Note the property "DataPropertyName".  
+				//The value of this property corresponds to the name of the property in the class Employee.  
+				//This binds this column to that field in the class.
+
+				InvoiceKeyColumn.DataPropertyName = "InvoiceKey";
+				InvoiceKeyColumn.HeaderText = "Invoice ID";
+				InvoiceKeyColumn.Name = "InvoiceID";
+
+				CustomerNameColumn.DataPropertyName = "customerName";
+				CustomerNameColumn.HeaderText = "Customer Full Name";
+				CustomerNameColumn.Name = "customerName";
+				//CustomerFirstNameColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+				//CustomerFirstNameColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+
+				SalesPersonName.DataPropertyName = "salesPersonName";
+				SalesPersonName.HeaderText = "Sales Person Name";
+				SalesPersonName.Name = "salesPersonName";
+
+				PurchaseDateColumn.DataPropertyName = "PurchaseDate";
+				PurchaseDateColumn.HeaderText = "Purchase Date";
+				PurchaseDateColumn.Name = "PurchaseDate";
+
+				CostColumn.DataPropertyName = "Cost";
+				CostColumn.HeaderText = "Total Cost";
+				CostColumn.Name = "Cost";
+
+				SelectInvoiceButtonColumn.HeaderText = "Select Invoice";
+				SelectInvoiceButtonColumn.Name = "SelectInvoice";
+				SelectInvoiceButtonColumn.Text = "Select the Invoice";
+				SelectInvoiceButtonColumn.UseColumnTextForButtonValue = true;
+				SelectInvoiceButtonColumn.Width = 200;
+
+				//Add the columns to the DataGridView
+				InvoiceDataGridView.Columns.Add(InvoiceKeyColumn);
+				InvoiceDataGridView.Columns.Add(CustomerNameColumn);
+				InvoiceDataGridView.Columns.Add(SalesPersonName);
+				InvoiceDataGridView.Columns.Add(PurchaseDateColumn);
+				InvoiceDataGridView.Columns.Add(CostColumn);
+				InvoiceDataGridView.Columns.Add(SelectInvoiceButtonColumn);
+
+				//Don't generate columns automatically
+				InvoiceDataGridView.AutoGenerateColumns = false;
+
+				//Set the DataSource for the DataGridView
+				InvoiceDataGridView.DataSource = Invoices;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+
+		public void fillInvoiceDropdown()
+		{
+			try
+			{
+				//clear out old data
+				cmbInvoiceNumber.Items.Clear();
+
+				da = new clsDataAccess(); //Data Access object to query database
+				DataSet ds; //Data set to store results of SQL statement
+				string sInvoiceSQL = "SELECT DISTINCT InvoiceKey FROM Invoices"; //SQL statement to execute
+
+				int iTotalResults = 0;  //Number of results that came back from SQL statement
+
+				//Run the statement and store the result into the dataset
+				ds = da.ExecuteSQLStatement(sInvoiceSQL, ref iTotalResults);
+
+				//For each row in the data set, add a new item to the combo box
+				foreach (DataRow dr in ds.Tables[0].Rows)
+					cmbInvoiceNumber.Items.Add(dr["InvoiceKey"].ToString());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+
+		public void fillDateDropdown()
+		{
+			try
+			{
+				//clear out old data
+				cmbInvoiceDate.Items.Clear();
+
+				da = new clsDataAccess(); //Data Access object to query database
+				DataSet ds; //Data set to store results of SQL statement
+				string sDateSQL = "SELECT DISTINCT PurchaseDate FROM Invoices"; //SQL statement to execute
+
+				int iTotalResults = 0;  //Number of results that came back from SQL statement
+
+				//Run the statement and store the result into the dataset
+				ds = da.ExecuteSQLStatement(sDateSQL, ref iTotalResults);
+
+				//For each row in the data set, add a new item to the combo box
+				foreach (DataRow dr in ds.Tables[0].Rows)
+					cmbInvoiceDate.Items.Add(dr["PurchaseDate"].ToString());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+
+		public void fillCostDropdown()
+		{
+
+			try
+			{
+				//clear out old data
+				cmbTotalCost.Items.Clear();
+
+				da = new clsDataAccess(); //Data Access object to query database
+				DataSet ds; //Data set to store results of SQL statement
+				string sCostSQL = "SELECT DISTINCT TotalCost FROM Invoices"; //SQL statement to execute
+
+				int iTotalResults = 0;  //Number of results that came back from SQL statement
+
+				//Run the statement and store the result into the dataset
+				ds = da.ExecuteSQLStatement(sCostSQL, ref iTotalResults);
+
+				//For each row in the data set, add a new item to the combo box
+				foreach (DataRow dr in ds.Tables[0].Rows)
+					cmbTotalCost.Items.Add(dr["TotalCost"].ToString());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+
+
+		#endregion
 
 		#region Button Methods
 
@@ -155,7 +220,57 @@ namespace _4780_final_car_POS
 			}
 		}
 
+		private void btnClearFilters_Click(object sender, EventArgs e)
+		{
+			//set the dropdown text to blank.
+			cmbTotalCost.Text = "";
+			cmbInvoiceDate.Text = "";
+			cmbInvoiceNumber.Text = "";
+
+			//Add in any new items by first clearing out the dropdown lists and then re-populating them
+			fillInvoiceDropdown();
+			fillDateDropdown();
+			fillCostDropdown();
+
+			//repopulate the datagridview with no filters.
+			InvoiceDataGridView.DataSource = DataControl.getAllInvoices();
+		}
+
+		private void InvoiceDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			try
+			{
+				//Grab the invoice record that was clicked on
+				Invoice invTemp = (Invoice)InvoiceDataGridView.Rows[e.RowIndex].DataBoundItem;
+
+				//Make sure there is an Invoice
+				if (invTemp != null)
+				{
+					//Determine if the "Select This Invoice" button was clicked
+					if (e.ColumnIndex == InvoiceDataGridView.Columns["SelectInvoice"].Index)
+					{
+						//Here's where I throw the invoice key to the Create invoice
+						int InvoiceKey = invTemp.InvoiceKey;
+						this.Hide();
+					}
+					else
+					{
+						////A row was selected so display the employee's information
+						//lblID.Text = invTemp.ID.ToString();
+						//lblFirstName.Text = invTemp.FirstName;
+						//lblLastName.Text = invTemp.LastName;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+			}
+		}
+
 		#endregion
+
+		#region FilterEvents
 
 		private void cmbInvoiceNumber_SelectedIndexChanged(object sender, EventArgs e)
 		{
@@ -202,7 +317,7 @@ namespace _4780_final_car_POS
 				cmbInvoiceDate.Text = "";
 				cmbInvoiceNumber.Text = "";
 				//get the string value selected and convert that into a decimal.
-				string sSelection = cmbInvoiceNumber.SelectedItem.ToString();
+				string sSelection = cmbTotalCost.SelectedItem.ToString();
 				decimal dSelection = Convert.ToDecimal(sSelection);
 				//use the int value of the value selected to repopulate the binding list
 				InvoiceDataGridView.DataSource = DataControl.getInvoicesByCost(dSelection);
@@ -213,50 +328,6 @@ namespace _4780_final_car_POS
 			}
 		}
 
-
-		private void InvoiceDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
-		{
-			try
-			{
-				//Grab the invoice record that was clicked on
-				Invoice invTemp = (Invoice)InvoiceDataGridView.Rows[e.RowIndex].DataBoundItem;
-
-				//Make sure there is an Invoice
-				if (invTemp != null)
-				{
-					//Determine if the "Select This Invoice" button was clicked
-					if (e.ColumnIndex == InvoiceDataGridView.Columns["SelectInvoice"].Index)
-					{
-						//Here's where I throw the invoice key to the Create invoice
-						int InvoiceKey = invTemp.InvoiceKey;
-						this.Hide();
-					}
-					else
-					{
-						////A row was selected so display the employee's information
-						//lblID.Text = invTemp.ID.ToString();
-						//lblFirstName.Text = invTemp.FirstName;
-						//lblLastName.Text = invTemp.LastName;
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-			}
-		}
-
-		private void btnClearFilters_Click(object sender, EventArgs e)
-		{
-			//set the dropdown text to blank.
-			cmbTotalCost.Text = "";
-			cmbInvoiceDate.Text = "";
-			cmbInvoiceNumber.Text = "";
-
-			//repopulate the datagridview with no filters.
-			InvoiceDataGridView.DataSource = DataControl.getAllInvoices();
-		}
-
-
+		#endregion
 	}
 }
