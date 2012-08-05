e8a4ddfb45ef2411931bf45f20d6a42aeebae9fe
sam.mtalbot@gmail.com <sam.mtalbot@gmail.com@fdc0112c-76aa-2afb-fc8e-ca9544c74a5f>
2012-08-05 12:29:07 +0000

    Just Adding frmSearch and frmSearchDesigner

--- branches/4780_final_car_POS/4780_final_car_POS/frmSearch.Designer.cs
+++ branches/4780_final_car_POS/4780_final_car_POS/frmSearch.Designer.cs
@@ -36,12 +36,14 @@
 			this.cmbInvoiceDate = new System.Windows.Forms.ComboBox();
 			this.lblTotalCost = new System.Windows.Forms.Label();
 			this.cmbTotalCost = new System.Windows.Forms.ComboBox();
+			this.lblFilters = new System.Windows.Forms.Label();
+			this.btnClearFilters = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.InvoiceDataGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// btnCancel
 			// 
-			this.btnCancel.Location = new System.Drawing.Point(654, 404);
+			this.btnCancel.Location = new System.Drawing.Point(683, 389);
 			this.btnCancel.Name = "btnCancel";
 			this.btnCancel.Size = new System.Drawing.Size(85, 23);
 			this.btnCancel.TabIndex = 0;
@@ -52,7 +54,7 @@
 			// InvoiceDataGridView
 			// 
 			this.InvoiceDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.InvoiceDataGridView.Location = new System.Drawing.Point(56, 91);
+			this.InvoiceDataGridView.Location = new System.Drawing.Point(56, 72);
 			this.InvoiceDataGridView.Name = "InvoiceDataGridView";
 			this.InvoiceDataGridView.Size = new System.Drawing.Size(683, 294);
 			this.InvoiceDataGridView.TabIndex = 1;
@@ -61,7 +63,7 @@
 			// cmbInvoiceNumber
 			// 
 			this.cmbInvoiceNumber.FormattingEnabled = true;
-			this.cmbInvoiceNumber.Location = new System.Drawing.Point(144, 47);
+			this.cmbInvoiceNumber.Location = new System.Drawing.Point(116, 35);
 			this.cmbInvoiceNumber.Name = "cmbInvoiceNumber";
 			this.cmbInvoiceNumber.Size = new System.Drawing.Size(121, 21);
 			this.cmbInvoiceNumber.TabIndex = 2;
@@ -70,7 +72,7 @@
 			// lblInvoiceNumber
 			// 
 			this.lblInvoiceNumber.AutoSize = true;
-			this.lblInvoiceNumber.Location = new System.Drawing.Point(53, 50);
+			this.lblInvoiceNumber.Location = new System.Drawing.Point(113, 19);
 			this.lblInvoiceNumber.Name = "lblInvoiceNumber";
 			this.lblInvoiceNumber.Size = new System.Drawing.Size(85, 13);
 			this.lblInvoiceNumber.TabIndex = 3;
@@ -79,7 +81,7 @@
 			// lblInvoiceDate
 			// 
 			this.lblInvoiceDate.AutoSize = true;
-			this.lblInvoiceDate.Location = new System.Drawing.Point(308, 50);
+			this.lblInvoiceDate.Location = new System.Drawing.Point(294, 19);
 			this.lblInvoiceDate.Name = "lblInvoiceDate";
 			this.lblInvoiceDate.Size = new System.Drawing.Size(71, 13);
 			this.lblInvoiceDate.TabIndex = 5;
@@ -88,15 +90,16 @@
 			// cmbInvoiceDate
 			// 
 			this.cmbInvoiceDate.FormattingEnabled = true;
-			this.cmbInvoiceDate.Location = new System.Drawing.Point(385, 47);
+			this.cmbInvoiceDate.Location = new System.Drawing.Point(297, 35);
 			this.cmbInvoiceDate.Name = "cmbInvoiceDate";
 			this.cmbInvoiceDate.Size = new System.Drawing.Size(121, 21);
 			this.cmbInvoiceDate.TabIndex = 4;
+			this.cmbInvoiceDate.SelectedIndexChanged += new System.EventHandler(this.cmbInvoiceDate_SelectedIndexChanged);
 			// 
 			// lblTotalCost
 			// 
 			this.lblTotalCost.AutoSize = true;
-			this.lblTotalCost.Location = new System.Drawing.Point(543, 50);
+			this.lblTotalCost.Location = new System.Drawing.Point(486, 19);
 			this.lblTotalCost.Name = "lblTotalCost";
 			this.lblTotalCost.Size = new System.Drawing.Size(69, 13);
 			this.lblTotalCost.TabIndex = 7;
@@ -105,16 +108,39 @@
 			// cmbTotalCost
 			// 
 			this.cmbTotalCost.FormattingEnabled = true;
-			this.cmbTotalCost.Location = new System.Drawing.Point(618, 47);
+			this.cmbTotalCost.Location = new System.Drawing.Point(489, 35);
 			this.cmbTotalCost.Name = "cmbTotalCost";
 			this.cmbTotalCost.Size = new System.Drawing.Size(121, 21);
 			this.cmbTotalCost.TabIndex = 6;
+			this.cmbTotalCost.SelectedIndexChanged += new System.EventHandler(this.cmbTotalCost_SelectedIndexChanged);
+			// 
+			// lblFilters
+			// 
+			this.lblFilters.AutoSize = true;
+			this.lblFilters.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFilters.Location = new System.Drawing.Point(25, 11);
+			this.lblFilters.Name = "lblFilters";
+			this.lblFilters.Size = new System.Drawing.Size(65, 24);
+			this.lblFilters.TabIndex = 8;
+			this.lblFilters.Text = "Filters:";
+			// 
+			// btnClearFilters
+			// 
+			this.btnClearFilters.Location = new System.Drawing.Point(645, 26);
+			this.btnClearFilters.Name = "btnClearFilters";
+			this.btnClearFilters.Size = new System.Drawing.Size(94, 37);
+			this.btnClearFilters.TabIndex = 9;
+			this.btnClearFilters.Text = "Clear Filters";
+			this.btnClearFilters.UseVisualStyleBackColor = true;
+			this.btnClearFilters.Click += new System.EventHandler(this.btnClearFilters_Click);
 			// 
 			// frmSearch
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(812, 449);
+			this.ClientSize = new System.Drawing.Size(800, 424);
+			this.Controls.Add(this.btnClearFilters);
+			this.Controls.Add(this.lblFilters);
 			this.Controls.Add(this.lblTotalCost);
 			this.Controls.Add(this.cmbTotalCost);
 			this.Controls.Add(this.lblInvoiceDate);
@@ -141,5 +167,7 @@
 	   private System.Windows.Forms.ComboBox cmbInvoiceDate;
 	   private System.Windows.Forms.Label lblTotalCost;
 	   private System.Windows.Forms.ComboBox cmbTotalCost;
+	   private System.Windows.Forms.Label lblFilters;
+	   private System.Windows.Forms.Button btnClearFilters;
     }
 }
